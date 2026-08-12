3ca39462eafa95417377cb24e3a80a7d6c76691b
Insert-Title-Here <krishpdoshi@gmail.com>
2026-08-12 13:32:04 -0700

    Content flashing through header

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+function handleScroll() {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
@@ -48,5 +48,8 @@ window.onscroll = () => {
     //animation footer on scroll
     let footer = document.querySelector('footer');
 
-    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+    footer.classList.toggle('show-animate', window.innerHeight + window.scrollY >= document.scrollingElement.scrollHeight);
 }
+
+window.onscroll = handleScroll;
+handleScroll();
